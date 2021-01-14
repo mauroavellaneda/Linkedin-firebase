ec6ae7dd94e635ab9692a5aceeae1519bd3b96d9
Mauro Avellaneda <mauroeavellaneda@gmail.com>
2021-01-14 16:57:24 -0300

    Setting up Redux and creating login flow

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { selectUser } from "./features/userSlice";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Feed from "./components/Feed/Feed";
+import Login from "./components/Login/Login";
 
 function App() {
+  const user = useSelector(selectUser);
+
   return (
     <div className="App">
       <Header />
-      <div className="app__body">
-        <Sidebar />
-        <Feed />
-      </div>
+
+      {user ? (
+        <Login />
+      ) : (
+        <div className="app__body">
+          <Sidebar />
+          <Feed />
+        </div>
+      )}
     </div>
   );
 }
